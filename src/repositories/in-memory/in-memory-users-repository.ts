31f063897c58a96624b0127786bd17c18cb39ba8
Bruno Simplicio <bruno@notabene.id>
2023-03-21 19:16:49 -0400

    Adding unit tests with InMemory repository

---
+++ src/repositories/in-memory/in-memory-users-repository.ts
@@ -0,0 +1,30 @@
+import { Prisma, User } from '@prisma/client'
+import { UsersRepository } from '../users-repository'
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = []
+
+  async findByEmail(email: string) {
+    const user = this.items.find((item) => item.email === email)
+
+    if (!user) {
+      return null
+    }
+
+    return user
+  }
+
+  async create({ name, email, password_hash }: Prisma.UserCreateInput) {
+    const user = {
+      id: 'user-1',
+      name,
+      email,
+      password_hash,
+      created_at: new Date(),
+    }
+
+    this.items.push(user)
+
+    return user
+  }
+}
