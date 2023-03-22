3391e8154ecd52955069c1e182660c34f022f948
Bruno Simplicio <bruno@notabene.id>
2023-03-22 18:44:01 -0400

    Get user profile id

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,35 @@
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { hash } from 'bcryptjs'
+import { expect, describe, it, beforeEach } from 'vitest'
+import { GetUserProfileUseCase } from './get-user-profile'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+let usersRepository: InMemoryUsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Get User Profile Use Case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(usersRepository)
+  })
+  it('should be able to get user profile', async () => {
+    const { id } = await usersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password_hash: await hash('123456', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: id,
+    })
+
+    expect(user.id).toEqual(expect.any(String))
+  })
+  it('should not be able to get user profile with wrong id', async () => {
+    await expect(
+      sut.execute({
+        userId: 'non-existent-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
