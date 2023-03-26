5f45900ed982f8442a720b818b9ee1e68453b151
Bruno Simplicio <bruno@notabene.id>
2023-03-26 14:29:38 -0400

    Gyms controller

---
+++ src/http/controllers/gyms/routes.ts
@@ -0,0 +1,7 @@
+import { FastifyInstance } from 'fastify'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+
+export async function gymsRoutes(app: FastifyInstance) {
+  // All routes under this will execute verifyJWT
+  app.addHook('onRequest', verifyJWT)
+}
