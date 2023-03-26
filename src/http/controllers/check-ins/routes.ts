d8f449198f5cf0a0c8029cc067c3d03f4b42456a
Bruno Simplicio <bruno@notabene.id>
2023-03-26 14:48:53 -0400

    Routes for checkin

--- src/http/controllers/check-ins/routes.ts
+++ src/http/controllers/check-ins/routes.ts
@@ -2,10 +2,15 @@ import { FastifyInstance } from 'fastify'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { create } from './create'
 import { validate } from './validate'
+import { metrics } from './metrics'
+import { history } from './history'
 
 export async function checkInRoutes(app: FastifyInstance) {
   // All routes under this will execute verifyJWT
   app.addHook('onRequest', verifyJWT)
+
+  app.get('/check-ins/history', history)
+  app.get('/check-ins/metrics', metrics)
 
   app.post('/gyms/:gymId/check-ins', create)
   app.patch('/check-ins/:checkInId/validate', validate)
